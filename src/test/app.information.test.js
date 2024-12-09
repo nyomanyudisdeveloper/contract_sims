455b62933557e9b587578b7849a7c7486b015a14
nyomanyudisdeveloper <nyomanyudisdeveloper@gmail.com>
2024-12-09 18:14:49 +0700

    unit test information services

--- src/test/app.information.test.js
+++ src/test/app.information.test.js
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import app from '../app.js';
 import { expectNoToken ,getToken} from './utils/requestTemplate.js';
-import { responseSchemaGetBannerSuccess } from './schema/schemaInformation.js';
+import { responseSchemaGetBannerSuccess, responseSchemaGetServiceSuccess } from './schema/schemaInformation.js';
 
 describe('API Test GET /banner', () => {
     it('GET /banner success', async() => {
@@ -17,5 +17,20 @@ describe('API Test GET /banner', () => {
         const response = await request(app).get('/banner')
         expectNoToken(response)
     })
+})
 
+describe('API Test GET /services', () => {
+    it('GET /services success', async() => {
+        const token = await getToken()
+        const response = await request(app).get('/services').set('Authorization',`Bearer ${token}`)
+        expect(response.statusCode).toBe(200)
+        const {error} = responseSchemaGetServiceSuccess.validate(response.body)
+        expect(error).toBeUndefined()
+
+    })
+
+    it('GET /services unauthorized', async () => {
+        const response = await request(app).get('/services')
+        expectNoToken(response)
+    })
 })
--- src/test/schema/schemaInformation.js
+++ src/test/schema/schemaInformation.js
@@ -12,3 +12,16 @@ export const responseSchemaGetBannerSuccess = Joi.object({
         })
     ).required()
 })
+
+export const responseSchemaGetServiceSuccess = Joi.object({
+    status: Joi.number().valid(0).required(),
+    message: Joi.string().valid("Sukses").required(),
+    data:Joi.array().items(
+        Joi.object({
+            service_code: Joi.string().required(),
+            service_name: Joi.string().required(),
+            service_icon: Joi.string().required(),
+            service_tarif: Joi.number().required()
+        })
+    ).required()
+})
